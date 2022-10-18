fe87a3d45b312cd7df7bf030d2454e6d69535f23
2464980p <2464980p@student.gla.ac.uk>
2022-10-18 17:52:25 +0100

    Added web scraping for entity abstracts

--- src/streamlit_app.py
+++
@@ -1,6 +0,0 @@
-import spacy
-from spacy_streamlit import visualize_ner
-
-nlp = spacy.load("en_core_web_sm")
-doc = nlp("Sundar Pichai is the CEO of Google.")
-visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
