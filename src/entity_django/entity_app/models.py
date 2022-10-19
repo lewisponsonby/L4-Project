9196a2c46a21874b7e235d1d188ea62d44260ba9
2464980p <2464980p@student.gla.ac.uk>
2022-10-19 17:04:07 +0100

    Added Models to django app

---
+++ src/entity_django/entity_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+import uuid
+# Create your models here.
+class Document(models.Model):
+    documentID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    text = models.TextField()
+
+class Entity(models.Model):
+    entityID = models.URLField(primary_key=True)
+    abstract = models.TextField()
+
+class Instance(models.Model):
+    instanceID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    documentID = models.ForeignKey('Document',on_delete=models.CASCADE)
+    entityID = models.ForeignKey('Entity', on_delete=models.CASCADE)
+    start = models.IntegerField()
+    stop = models.IntegerField()
+
